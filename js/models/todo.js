00488401fc339fd348868fc82b62e1e11ef50ba4
Kun Ma <kma@chegg.com>
2018-03-06 17:33:56 -0800

    Added css, todo view, app view, etc.

---
+++ js/models/todo.js
@@ -0,0 +1,16 @@
+var app = app || {};
+
+app.Todo = Backbone.Model.extend({
+    // Default attributes ensure that each todo created has `title` and `completed` keys.
+    defaults: {
+      title: '',
+      completed: false
+    },
+
+    // Toggle the `completed` state of this todo item.
+    toggle: function() {
+      this.save({
+        completed: !this.get('completed')
+      });
+    }
+  });
